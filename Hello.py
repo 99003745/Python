06463aa915c41be92f51669cfefcb045e0d23480
99003745 <ujjawal.kumar@ltts.com>
2021-02-16 14:03:42 +0530

    updated

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
-print("pawry ho rahi hai")
-print("rajat gussa hai")
-print("pawry from linux vs code")
+print("win from vs code")
+print("win from pycharm")
+print("from linux vs code")
+print("from linus pycharm")
